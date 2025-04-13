0ff661c4bde5df947b59f343ca4361d534db5832
Alejandro <alejandro12850mj@gmail.com>
2025-04-12 23:46:21 -0400

    database

--- src/api/models.py
+++ src/api/models.py
@@ -1,33 +1,47 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean, Text, ForeignKey, Table, Column
-from sqlalchemy.orm import Mapped, mapped_column, relationship, DeclarativeBase
+from sqlalchemy import String, Boolean, Text, ForeignKey, Table 
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from typing import List
 
-class Base(DeclarativeBase):
-    pass
+db = SQLAlchemy()
 
 
-association_table = Table(
-    "association_table",
-    Base.metadata,
-    Column("left_id", ForeignKey("left_table.id"), primary_key=True),
-    Column("right_id", ForeignKey("right_table.id"), primary_key=True),
-)
-
-
-class Parent(Base):
-    __tablename__ = "left_table"
-
+class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    children: Mapped[List[Child]] = relationship(
-        secondary=association_table, back_populates="parents"
-    )
+    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(nullable=False)
+    salt: Mapped[str] = mapped_column(String(255), nullable=True)
+
+    project: Mapped['Project'] = relationship(back_populates='user')
 
 
-class Child(Base):
-    __tablename__ = "right_table"
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            # "projects": list(map(lambda item: item.serialize(), self.project))    
+        }
 
+
+class Project(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    parents: Mapped[List[Parent]] = relationship(
-        secondary=association_table, back_populates="children"
-    )
+    title: Mapped[str] = mapped_column(String(200), nullable=False)
+    description: Mapped[str] = mapped_column(Text(), nullable=False)
+    in_progress: Mapped[bool] = mapped_column(Boolean(False), nullable=False)
+
+
+    user_id: Mapped[int] = mapped_column(int(), ForeignKey('user.id'))
+    user: Mapped[User] = relationship(back_populates='project')
+
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "in_progress": self.in_progress,
+            # "user_id": self.user_id,
+            # "users": self.user.serialize()
+        }
+
+
